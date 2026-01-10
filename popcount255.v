2b59b0b903a67023888d21d17862af7a0455ad9c
unknown <dandidesta82@gmail.com>
2026-01-10 02:55:51 -0500

    HDLbits: Popcount255 - Logic

--- popcount255.v
+++ popcount255.v
@@ -1,3 +1,13 @@
 module top_module( 
     input [254:0] in,
     output [7:0] out );
+    
+    always @(*) begin
+        out = 8'b0;
+        for (int i =0; i < 255; i = i +1) begin
+            out = in[i] + out; //out = (in[i] == 1'b1) ? out + 1'b1 : out; also works but more hardware(comparators)
+            // are created making the circuit expensive.
+        end
+    end      
+​
+endmodule
