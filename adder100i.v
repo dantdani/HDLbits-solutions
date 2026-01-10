5d5bbaa43554841eb57b9c31fa0febb1c880e67d
unknown <dandidesta82@gmail.com>
2026-01-10 02:56:41 -0500

    HDLbits: Adder100i - Logic

--- adder100i.v
+++ adder100i.v
@@ -3,3 +3,27 @@ module top_module(
     input cin,
     output [99:0] cout,
     output [99:0] sum );
+   
+   
+        wire [100:0] couts;
+        assign couts[0] = cin;
+        genvar i;
+        generate 
+            for (i = 0; i < 100; i = i +1) begin: adders
+                add yes(.a(a[i]),
+                        .b(b[i]),
+                        .cin(couts[i]),
+                        .sum(sum[i]),
+                        .cout(couts[i + 1]));
+            end
+        endgenerate
+    assign cout = couts[100:1]; // chop off cin
+                      
+​
+endmodule
+                        
+module add(input a,b,cin,
+           output sum, cout);
+    assign sum = a ^ b ^ cin;
+    assign cout = (a&b) | (b&cin) | (a&cin);
+endmodule
