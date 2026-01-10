d586ea4ead7d58052e345f786d458315befa7b19
unknown <dandidesta82@gmail.com>
2026-01-10 14:33:22 -0500

    HDLbits: TruthTable1 - Logic

--- truthtable1.v
+++ truthtable1.v
@@ -4,3 +4,11 @@ module top_module(
     input x1,  // three inputs
     output f   // one output
 );
+    wire a1, a2, a3, a4;
+    and first(a1, ~x1, x2, ~x3);
+    and sec(a2, x1, x2, ~x3);
+    and third(a3, x1, ~x2, x3);
+    and last(a4, x1, x2, x3);
+    or out(f, a1, a2, a3,a4);
+​
+endmodule
