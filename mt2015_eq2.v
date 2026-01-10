c44480c61668be534cc40c25d0d5fe3e79c3b5ca
unknown <dandidesta82@gmail.com>
2026-01-10 15:02:44 -0500

    HDLbits: Mt2015 eq2 - Logic

--- mt2015_eq2.v
+++ mt2015_eq2.v
@@ -1,1 +1,7 @@
 module top_module ( input [1:0] A, input [1:0] B, output z ); 
+    //simple comparator
+    wire [1:0] sum;
+    assign sum = A - B;
+    assign z = ~(| sum); //bit wise or is 0 when all are zero implying equality! 
+​
+endmodule 
