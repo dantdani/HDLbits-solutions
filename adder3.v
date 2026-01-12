819ddd8e924ebe6cf5d3b8b422ee8d8b0abd1173
unknown <dandidesta82@gmail.com>
2026-01-12 00:38:13 -0500

    HDLbits: Adder3 - Logic

--- adder3.v
+++ adder3.v
@@ -4,4 +4,15 @@ module top_module(
     output [2:0] cout,
     output [2:0] sum );
 
+    fadd inst0(a[0], b[0], cin, cout[0], sum[0]);
+    fadd inst1(a[1], b[1], cout[0], cout[1], sum[1]);
+    fadd inst2(a[2], b[2], cout[1], cout[2], sum[2]);
+
+endmodule
+
+module fadd( 
+    input a, b, cin,
+    output cout, sum );
+    assign sum = a ^ b ^ cin;
+    assign cout = a&b | a&cin | b&cin;
 endmodule
